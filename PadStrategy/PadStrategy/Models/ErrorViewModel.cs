30d7c7349db3cff7eb9577d8df8f1ef113db3ea3
勇翔 王 <Wang.Yung-Hsiang@outlook.com>
2018-09-19 22:32:33 +0800

    MVC專案建立

---
+++ PadStrategy/PadStrategy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PadStrategy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
